996e09466b76abb33bdc5a2fd2a8f4cccc6b480e
Sam Fales <samgfales@gmail.com>
2022-06-20 21:20:44 -0600

    Fixed post method

--- api/test.py
+++ api/test.py
@@ -1,3 +1,8 @@
 import requests
 resp = requests.get('http://127.0.0.1:8000/noahsGirlfriend').json()
 print(resp['message'])
+
+url = 'http://127.0.0.1:8000/vrPost'
+json = {"left":"true", "right":"true", "forward":"true", "back":"true", "lookLeft":"true", "lookRight":"true", "lookDown":"true", "lookUp":"true"}
+resp = requests.post(url = url, json = json)
+print(resp.json())
