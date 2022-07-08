125ea7b2e0a4ddd0ed2e1008ce82ee46fb70625c
Sam <67492097+sammyoto@users.noreply.github.com>
2022-07-08 01:54:59 -0600

    Update piToApi.py

--- piToApi.py
+++ piToApi.py
@@ -3,35 +3,38 @@ import requests
 import time
 from gpiozero import LED
 
+#initialize the IO ports
 rX = LED(17)
 rY = LED(19)
-lX = LED(16)
-lY = LED(20)
+lY = LED(16)
+lX = LED(20)
 
+#run while pi is on
 while True:
     resp = requests.get('https://wfevbii2w47dcqjn3tkwyf6evq0pbckw.lambda-url.us-west-2.on.aws/piGet')
     json_dict = json.loads(resp.json())
     print(json_dict)
     
+#if there is nothing in the queue
     if len(json_dict.keys()) < 2:
         print(json_dict["message"])
-    else:  
-        if json_dict["handy"] == "Right":
-            if float(json_dict["stickX"]) > 0.3:
-                rX.on()
-            if float(json_dict["stickY"]) > 0.3:
-                rY.on()
-        
+
+#else parse JSON for left stick data
+    else:
         if json_dict["handy"] == "Left":
-            if float(json_dict["stickX"]) > 0.3:
+            if float(json_dict["stickX"]) > 0.5:
                 lX.on()
-            if float(json_dict["stickY"]) > 0.3:
+            if float(json_dict["stickY"]) > 0.5:
                 lY.on()
-    
-    time.sleep(0.125)
-    rX.off()
-    rY.off()
-    lX.off()
-    lY.off()
-    
-    
+            if float(json_dict["stickX"]) < -0.5:
+                rX.on()
+            if float(json_dict["stickY"]) < -0.5:
+                rY.on()
+            if float(json_dict["stickX"]) < 0.5 and float(json_dict["stickX"]) > -0.5:
+                lX.off()
+                rX.off()
+            if float(json_dict["stickY"]) < 0.5 and float(json_dict["stickY"]) > -0.5:
+                lY.off()
+                rY.off()
+#turn on led/motor if stick is past a treshhold, turn it off otherwise
+    time.sleep(0.05)
